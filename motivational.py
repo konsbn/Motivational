12b14eae9124ed7ef86430f98b8d3f9a2ddd04a3
William Sankey <WSankey@gmail.com>
2014-12-01 21:56:01 -0500

    Updated motivational program to retrieve reddit comments off of /r/GetMotivated

--- motivational.py
+++ motivational.py
@@ -1,5 +1,10 @@
+#Comments
 import praw
 
 user_agent = ("Motivational sayings 1.0 by /u/DistrictRails")
 r = praw.Reddit(user_agent=user_agent)
 
+subreddit = r.get_subreddit('getmotivated')
+subreddit_comments = subreddit.get_comments()
+for i in subreddit_comments:
+    print i
