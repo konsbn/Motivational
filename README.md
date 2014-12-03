e8b9163aa2af27d8a52f8dff8d545bc7afa91a9c
William Sankey <WSankey@gmail.com>
2014-12-02 23:20:03 -0500

    Added readme to repository.

---
+++ README.md
@@ -0,0 +1,12 @@
+Motivational Sayings Generator
+
+The idea for this script is to generate motivational sayings.
+To do this I attempt to collect data from Reddit's 'Get
+Motivated' subreddit, store it in a database, and parse back the
+results. Another future edit will be to use motivational and
+inspiring works found on Project Gutenberg, and other sites, to
+generate sayings. 
+
+Please let me know your thoughts and if you have some other ideas for
+how to do this...most helpful are thoughts around using Python's
+NLTK capabilities. 
--- motivational.py
+++ motivational.py
@@ -1,10 +1,16 @@
-#Comments
-import praw
+#A script to analyze Reddit's Get Motivated subreddit
+import praw #This is Reddit's API
+import nltk
 
+#We need to set the user_agent for Reddit
 user_agent = ("Motivational sayings 1.0 by /u/DistrictRails")
 r = praw.Reddit(user_agent=user_agent)
 
+#Obtain the comments from GetMotivated
 subreddit = r.get_subreddit('getmotivated')
 subreddit_comments = subreddit.get_comments()
-for i in subreddit_comments:
-    print i
+
+tokens = nltk.word_tokenize(subreddit_comments)
+#for i in subreddit_comments:
+#    print i
+
