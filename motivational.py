1610e2e2f29826d4d2edaa97b5c933fd65a2af85
William Sankey <WSankey@gmail.com>
2014-12-08 21:28:22 -0500

    Parsing data to text file

--- motivational.py
+++ motivational.py
@@ -1,16 +1,44 @@
 #A script to analyze Reddit's Get Motivated subreddit
+#Still in the works but getting there...
 import praw #This is Reddit's API
-import nltk
+import json
+import os
 
 #We need to set the user_agent for Reddit
 user_agent = ("Motivational sayings 1.0 by /u/DistrictRails")
 r = praw.Reddit(user_agent=user_agent)
-
 #Obtain the comments from GetMotivated
 subreddit = r.get_subreddit('getmotivated')
-subreddit_comments = subreddit.get_comments()
+comments = subreddit.get_comments()
+#flat_comments = praw.helpers.flatten_tree(comments)
 
-tokens = nltk.word_tokenize(subreddit_comments)
-#for i in subreddit_comments:
-#    print i
 
+f = open('output.txt', 'w')
+words = {}
+for comment in comments:
+    for word in comment.body.split(" "):
+        if word in words.keys():
+            words[word] += 1
+        else:
+            words[word] = 0
+    print(json.dumps(words, f))
+    words.clear()
+    f.flush()
+f.close()
+
+'''
+file_name = "data_dump"
+with open(file_name, 'w') as outfile:
+    json.dump(data, outfile)
+    print "Page {}".format(i+1:)
+
+
+fo = open('foo.txt', "rw+")
+print "Name of the file: ", fo.name
+
+
+
+src = StringIO()
+p = pickle.Pickler(src)
+p.dump(comments)
+'''
